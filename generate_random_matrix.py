3281336199aa5d1ce73850d7ed7d5c86d0071689
Chen Yu <chenyyuio@gmail.com>
2014-02-28 11:20:49 +0000

    temp

---
+++ generate_random_matrix.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+import numpy as np
+import itertools as it 
+
+
+np.random.seed(3)
+def generate(seq,N):
+    L = len(seq)
+    # Use number 0,1,2,3 as S,M,R,T
+    Table = np.random.random_integers(0,L-1,size=(N,N))
+    Table2 = np.chararray((N,N))
+    for i in range(N):
+        for j in range(N):
+            Table2[i,j] = seq[Table[i,j]]
+    np.savetxt(seq + ".txt", Table2, fmt="%s")
+
+
+generate("SMRT",100)
+generate("FISHEAD",10000)
