05859138fcafa39d1840ccfd5354fcfb4e0f9f94
Chen Yu <chenyu.nue@gmail.com>
2014-02-28 16:17:49 +0800

    version3

--- smrt.py
+++ smrt.py
@@ -1,10 +1,11 @@
+from __future__ import print_function
 import numpy as np
 
 
 seq = list("SMRT")
 L = len(seq)
 index_seq = dict(zip(seq, range(L)))
-N = 5
+N = 100
 Table = []
 for i in range(N):
     Table.append([])
@@ -12,27 +13,26 @@ for i in range(N):
     for j in range(N):
         c = seq[np.random.random_integers(0,L-1)]
         Table[j].append(c)
-print(Table)
+#for line in Table:
+#    print(line,sep='\n')
 
 
 def checkseq(i,j):
-    iUL = index_seq[Table[i][j]]
-    iUR = index_seq[Table[i][j+1]]
-    iBL = index_seq[Table[i+1][j]]
-    iBR = index_seq[Table[i+1][j+1]]
-    if iUR-iUL % 4 != 1:
-        return False
-    elif iBR-iUR % 4 != 1:
-        return False
-    elif iBL-iBR % 4 != 1:
-        return False
-    elif iUL-iBL % 4 != 1:
         return True
 
 Result = []
 for i in range(N-1):
-    Result.append([])
-for i in range(N-1):
     for j in range(N-1):
-        Result[i].append(checkseq(i,j))
+        iUL = index_seq[Table[i][j]]
+        iUR = index_seq[Table[i][j+1]]
+        iBL = index_seq[Table[i+1][j]]
+        iBR = index_seq[Table[i+1][j+1]]
+        if iUR-iUL % 4 != 1:
+            continue
+        elif iBR-iUR % 4 != 1:
+            continue
+        elif iBL-iBR % 4 != 1:
+            continue
+        elif iUL-iBL % 4 != 1:
+            Result.append((i,j))
 print(Result)
